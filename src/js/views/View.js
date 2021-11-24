e320aadd74a08aafc04ffe19e8c586fcc74ee45b
gigagogashvili2003 <g.gogashvili2003@gmail.com>
2021-11-24 14:17:15 +0400

    final commit

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,12 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   * Render the recived object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string isted of rendering to the DOM
+   * @returns
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
